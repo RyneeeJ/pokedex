a1a1ee6e9e608b74b926bfb130cd08cfb15d24c1
RyneeeJ <imrynegandia@gmail.com>
2026-06-08 04:43:21 +0800

    Add dummy links to footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/layout/Footer";
 
 export default function App() {
   return (
-    <div className="mx-auto flex min-h-screen max-w-(--container-width) flex-col">
+    <div className="mx-auto flex min-h-screen max-w-(--container-width) flex-col text-(--text-primary)">
       <Header />
       <main className="flex-1">hello world</main>
       <Footer />
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,20 @@
 export default function Footer() {
+  const dummyLinksClass =
+    "text-sm text-(--text-secondary) hover:underline cursor-pointer";
   return (
-    <div className="flex min-h-[80px] flex-col justify-center border-t border-(--border) bg-(--footer-bg) px-6 shadow-xs">
-      <span>Pokèdex Encyclopedia</span>
-      <span className="text-sm text-(--text-secondary)">
-        &copy; {new Date().getFullYear()} Pokèdex. All rights reserved.
-      </span>
+    <div className="flex min-h-[88px] items-center justify-between border-t border-(--border) bg-(--footer-bg) px-6 shadow-xs">
+      <div className="flex flex-col justify-center gap-1">
+        <span className="text-lg font-semibold">Pokèdex Encyclopedia</span>
+        <span className="text-sm text-(--text-secondary)">
+          &copy; {new Date().getFullYear()} Pokèdex. All rights reserved.
+        </span>
+      </div>
+
+      <ul className="flex items-center gap-6">
+        <li className={dummyLinksClass}>Privacy Policy</li>
+        <li className={dummyLinksClass}>Terms of Service</li>
+        <li className={dummyLinksClass}>Contact Us</li>
+      </ul>
     </div>
   );
 }
