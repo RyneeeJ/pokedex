721c68a3c611f70867bbe3f8fb5419771c989b18
RyneeeJ <imrynegandia@gmail.com>
2026-07-25 16:29:03 +0800

    fix minor lint warnings

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const dummyLinksClass =
     "text-sm text-(--text-secondary) hover:underline cursor-pointer";
   return (
-    <div className="flex min-h-[88px] items-center justify-between border-t border-(--border) bg-(--footer-bg) px-6 shadow-xs">
+    <div className="border-border flex min-h-22 items-center justify-between border-t bg-(--footer-bg) px-6 shadow-xs">
       <div className="flex flex-col justify-center gap-1">
         <span className="text-lg font-semibold">Pokèdex Encyclopedia</span>
         <span className="text-sm text-(--text-secondary)">
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,12 @@ import SearchBar from "../search/SearchBar";
 
 export default function Header() {
   return (
-    <div className="flex min-h-[64px] items-center justify-between border-b border-(--border-light) px-6 shadow-xs">
+    <div className="flex min-h-16 items-center justify-between border-b border-(--border-light) px-6 shadow-xs">
       <a href="/" className="text-primary text-2xl font-bold">
         Pokèdex
       </a>
 
-      <div className="w-full max-w-[300px]">
+      <div className="w-full max-w-75">
         <SearchBar variant="header" />
       </div>
     </div>
