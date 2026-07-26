1b802e0e3d9572d20132861115d3d3ef1b26f684
RyneeeJ <imrynegandia@gmail.com>
2026-07-26 19:57:51 +0800

    improve formatting of base stats names

--- src/components/pokemon/BaseStatsField.tsx
+++ src/components/pokemon/BaseStatsField.tsx
@@ -13,9 +13,25 @@ export default function BaseStatsField({
     | "speed";
   value: number;
 }) {
+  let formattedStat = "";
+  if (stat === "hp") {
+    formattedStat = "HP";
+  } else if (stat === "attack") {
+    formattedStat = "Attack";
+  } else if (stat === "defense") {
+    formattedStat = "Defense";
+  } else if (stat === "specialattack") {
+    formattedStat = "SP. ATK";
+  } else if (stat === "specialdefense") {
+    formattedStat = "SP. DEF";
+  } else if (stat === "speed") {
+    formattedStat = "Speed";
+  }
   return (
     <div className="flex items-center justify-between">
-      <div className="w-1/5 text-xs font-medium text-[#855300]">{stat}</div>
+      <div className="w-1/5 text-xs font-medium text-[#855300]">
+        {formattedStat}
+      </div>
       <div className="flex-1">
         <StatsBar value={value} stat={stat} />
       </div>
